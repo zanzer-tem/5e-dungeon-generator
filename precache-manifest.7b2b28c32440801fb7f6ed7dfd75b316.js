72e914598842ae43d9ec47a3918f73046a8b9527
dan-at-jisc <dan.needham@jisc.ac.uk>
2020-06-28 08:29:20 +0100

    Updates

---
+++ precache-manifest.7b2b28c32440801fb7f6ed7dfd75b316.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d84783b1300df460c486487500646bbc",
+    "url": "/5e-dungeon-generator/index.html"
+  },
+  {
+    "revision": "46fdbe71c714d4691500",
+    "url": "/5e-dungeon-generator/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "34cbe350415eff9e746d",
+    "url": "/5e-dungeon-generator/static/js/2.cb12af97.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/5e-dungeon-generator/static/js/2.cb12af97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46fdbe71c714d4691500",
+    "url": "/5e-dungeon-generator/static/js/main.5e6a3728.chunk.js"
+  },
+  {
+    "revision": "734b58c5e1bd5aa57e39",
+    "url": "/5e-dungeon-generator/static/js/runtime-main.27df41a2.js"
+  }
+]);
